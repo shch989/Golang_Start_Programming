4793d67f52a31f27b03a251b5d115691af81827d
shch989 <tankpop11@gmail.com>
2023-07-19 22:17:11 +0900

    hello world 예제

--- ch01/ex01/main.go
+++ ch01/ex01/main.go
@@ -5,5 +5,23 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, playground")
+	fmt.Println("Hello everyone, this is the most awesome class in the entire world for having fun and learning the GO programming language.")
+	foo()
+	fmt.Println("something more")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+
+	bar()
+}
+
+func foo() {
+	fmt.Println("I'm in foo")
+}
+
+func bar() {
+	fmt.Println("and then we exited")
 }
