2861262a1af43c4a3979c0971e6b38cd95d49cbf
shch989 <tankpop11@gmail.com>
2023-07-21 01:21:54 +0900

    실습 예제#1

---
+++ qz01/ex01/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// Qz01 단축 선언 연산자를 사용해서 이 값을 식별자가 x, y, z인 변수에 지정하십시오
+// a. 42
+// b. "James Bond"
+// c. true
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
