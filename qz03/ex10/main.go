ee8caa5d2ddc13d750bd4697e057891a4d12d39d
shch989 <tankpop11@gmail.com>
2023-07-28 02:23:23 +0900

    실습예제 #10

---
+++ qz03/ex10/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// 논리연산자를 사용하여 출력하여라
+
+func main() {
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true && false)
+	fmt.Println(!true)
+	fmt.Println(!false)
+}
