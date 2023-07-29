e0ad61a836645d4e040feffa5f56101250570555
shch989 <tankpop11@gmail.com>
2023-07-29 23:54:02 +0900

    실습예제 #10

---
+++ qz04/ex10/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// 이전 실습에서 맵에 항목을 삭제한다.
+// range를 이용해 맵을 출력한다.
+
+func main() {
+	m := map[string][]string{
+		`bond_james`:      {`Shaken, not stirred`, `Martinis`, `Women`},
+		`moneypenny_miss`: {`James Bond`, `Literature`, `Computer Science`},
+		`no_dr`:           {`Being evil`, `Ice cream`, `Sunsets`},
+	}
+
+	m[`fleming_lan`] = []string{`steaks`, `clgars`, `espionage`}
+
+	delete(m, `no_dr`)
+
+	for k, v := range m {
+		fmt.Println("This is the record for", k)
+		for i, v2 := range v {
+			fmt.Println("\t", i, v2)
+		}
+	}
+}
