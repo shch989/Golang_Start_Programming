caa9d7bd32a4100c4f7de03e6de27489b3818efd
shch989 <tankpop11@gmail.com>
2023-08-05 02:23:15 +0900

    포인터란 무엇인가?

---
+++ ch07/ex01/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 42
+	fmt.Println(a)
+	fmt.Println(&a)
+
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", &a)
+
+	b := &a
+	fmt.Println(b)
+	fmt.Println(*b)
+	fmt.Println(*&a)
+
+	*b = 43
+	fmt.Println(a)
+}
