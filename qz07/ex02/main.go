8c39ef13323cb2bc70ffad1e34e0cc8f58914c37
shch989 <tankpop11@gmail.com>
2023-08-06 00:54:33 +0900

    실습과제 #2

--- qz07/ex02/main.go
+++ qz07/ex02/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+type person struct {
+	name string
+}
+
 func main() {
-	fmt.Println()
+	p1 := person{
+		name: "James Bond",
+	}
+	fmt.Println(p1)
+	changeMe(&p1)
+	fmt.Println(p1)
+}
+
+func changeMe(p *person) {
+	p.name = "Miss Moneypenny"
 }
