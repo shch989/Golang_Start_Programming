c11c5a21027aa53793a2a2c36a2f7345e000f2e0
shch989 <tankpop11@gmail.com>
2023-08-09 23:26:07 +0900

    실습예제 #2

---
+++ qz08/ex02/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type person struct {
+	First   string   `json: "First"`
+	Last    string   `json: "Last"`
+	Age     int      `json: "Age"`
+	Sayings []string `json: "Sayings"`
+}
+
+func main() {
+	s := `[{"First": "James", "Last": "Bond", "Age": 32, "Sayings": ["Hello", "Byebye"]}, {"First": "James", "Last": "Bond", "Age": 32, "Sayings": ["Hello", "Byebye"]}, {"First": "James", "Last": "Bond", "Age": 32, "Sayings": ["Hello", "Byebye"]}]`
+	fmt.Println(s)
+
+	var people []person
+
+	err := json.Unmarshal([]byte(s), &people)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(people)
+
+	for i, person := range people {
+		fmt.Println("Person #", i)
+		fmt.Println("\t", person.First, person.Last, person.Age)
+		for _, saying := range person.Sayings {
+			fmt.Println("\t\t", saying)
+		}
+	}
+}
