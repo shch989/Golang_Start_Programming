792737c6c66c089e40d67d8f3014e15ecd266f54
shch989 <tankpop11@gmail.com>
2023-08-09 22:32:02 +0900

    bcrypt

---
+++ ch08/ex06/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+	// 원래 비밀번호
+	s := "password123"
+	// 비밀번호 해시 생성
+	bs, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.MinCost)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(s)
+	fmt.Println(bs)
+
+	// 로그인 시도할 비밀번호
+	loginPword1 := `password123`
+
+	// 비밀번호 해시와 로그인 시도한 비밀번호 비교
+	err = bcrypt.CompareHashAndPassword(bs, []byte(loginPword1))
+	if err != nil {
+		fmt.Println("로그인할 수 없습니다")
+		return
+	}
+	fmt.Println("로그인되었습니다")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module shch989/Golang_Start_Programming
 
 go 1.20
+
+require (
+	github.com/aerospike/aerospike-client-go v4.5.2+incompatible
+	golang.org/x/crypto v0.12.0
+)
